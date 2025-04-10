69e527447eb4abcbbceb45ef743d109e180bea9b
Mark Albrand <mark.albrand5@gmail.com>
2025-04-09 22:41:22 -0600

    Envio como JSOn

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel, EmailStr, Field
 
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
 class UserBase(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=8)
